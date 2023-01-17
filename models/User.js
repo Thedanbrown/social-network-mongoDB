445a72c6d6bfba914f62871815cb2bde26d1f34e
[Dan Brown] <[thedanbrown@gmail.com]>
2023-01-16 20:28:21 -0500

    models and config

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please enter a valid email address"],
+    },
+    thoughts: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+        },
+    ],
+    friends: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        },
+    ],
+    },
+    {
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+module.exports = User;
